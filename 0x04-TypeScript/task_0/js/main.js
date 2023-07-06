ce2009927cdc2e124a9c35167c76a2aea330a11b
Sammie2022 <samuelnganganganga26@gmail.com>
2023-07-06 03:46:17 -0700

    Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,31 @@
+var student1 = {
+    firstName: "Jackline",
+    lastName: "Wanjiku",
+    age: 32,
+    location: "Nairobi"
+};
+var student2 = {
+    firstName: "Chancie",
+    lastName: "Nyokabi",
+    age: 25,
+    location: "Kikuyu"
+};
+var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
+table.style.background = "pink";
+table.appendChild(tbody);
+studentsList.forEach(function (student) {
+    var row = document.createElement('tr');
+    var nameCell = document.createElement('td');
+    var locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    nameCell.style.border = "1px solid pink";
+    locationCell.style.border = "1px solid pink";
+    nameCell.style.padding = "5px";
+    locationCell.style.padding = "5px";
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
